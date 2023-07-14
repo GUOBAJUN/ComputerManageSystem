e1b5ece153402a1cd827c672af700473864abafe
GUOBAJUN <guobajun@gmail.com>
2023-07-14 10:54:47 +0800

    更新ip直连为mDNS.local

--- Client/AgentCLI.py
+++ Client/AgentCLI.py
@@ -3,7 +3,7 @@ import time
 import threading
 
 if __name__ == '__main__':
-    agent = Agent.CollectingAgent('http://192.168.43.82:10086/')
+    agent = Agent.CollectingAgent('http://desktop-i6jagl6.local:10086/')
     agent.report_system_info()
     while True:
         agent.report_performance()
